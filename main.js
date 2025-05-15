6f65c1023c5adf7e4399cabfddec48f68f601340
Bakdoolot <bakdoolottoychubekov@gmail.com>
2025-05-16 00:01:02 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const jsonUrl = 'https://raw.githubusercontent.com/tcybkvv/dosbek_data/refs/heads/main/data/info.json';
+
+fetch(jsonUrl)
+    .then(res => res.json())
+    .then(data => {
+        const container = document.getElementById('container');
+        container.innerHTML = '';
+
+        data.forEach(post => {
+            const card = document.createElement('div');
+            card.className = 'card';
+
+            card.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.description}</p>
+            <div class="tags">${post.tags.join(', ')}</div>
+            <small>Создано: ${new Date(post.created_at).toLocaleDateString()}</small>
+          `;
+
+            container.appendChild(card);
+        });
+    })
+    .catch(error => {
+        document.getElementById('container').innerText = 'Ошибка загрузки данных: ' + error;
+    });
