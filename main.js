fbf516e58679dfc4ae2cbbfb7fc56c7f3919d06e
Bakdoolot <bakdoolottoychubekov@gmail.com>
2025-05-16 12:16:31 +0600

    third commit

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 const jsonUrl = 'https://raw.githubusercontent.com/tcybkvv/dosbek_data/refs/heads/main/data/info.json'
 
-const loader = document.getElementById('loader');
-const container = document.getElementById('container');
-
-loader.style.display = 'block';
-
 fetch(jsonUrl)
     .then(res => res.json())
     .then(data => {
+        const container = document.querySelector('#main__container');
         container.innerHTML = '';
 
         data.forEach(post => {
@@ -15,18 +11,15 @@ fetch(jsonUrl)
             card.className = 'card';
 
             card.innerHTML = `
-                <h2>${post.title}</h2>
-                <p>${post.description}</p>
-                <div class="tags">${post.tags.join(', ')}</div>
-                <small>Создано: ${new Date(post.created_at).toLocaleDateString()}</small>
-            `;
+            <h2>${post.title}</h2>
+            <p>${post.description}</p>
+            <div class="tags">${post.tags.join(', ')}</div>
+            <small>Создано: ${new Date(post.created_at).toLocaleDateString()}</small>
+          `;
 
             container.appendChild(card);
         });
-
-        loader.style.display = 'none';
     })
     .catch(error => {
-        container.innerText = 'Ошибка загрузки данных: ' + error;
-        loader.style.display = 'none';
+        document.getElementById('container').innerText = 'Ошибка загрузки данных: ' + error;
     });
